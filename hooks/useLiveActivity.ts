03c4a640cde7c86c8e5baefc041c7a9f3b6c79f9
Petr Horacek <petrhoracekdev@gmail.com>
2025-03-30 10:57:52 +0200

    add docs to core functions

--- hooks/useLiveActivity.ts
+++ hooks/useLiveActivity.ts
@@ -3,11 +3,25 @@ import { endLiveActivity, startLiveActivity } from "@/utils/LiveActivity";
 
 import { MMKV } from "react-native-mmkv";
 
+/**
+ * Storage instance using MMKV for storing live activity ID.
+ */
 export const Storage = new MMKV();
 
+/**
+ * Key used to store the live activity ID in storage.
+ */
 const liveActivityKey = "liveActivityId";
 
+/**
+ * Custom hook to manage live activities.
+ *
+ * @returns {Object} An object containing functions to start and end live activities.
+ */
 const useLiveActivity = () => {
+  /**
+   * Starts a live activity and stores its ID in storage.
+   */
   const handleStartActivity = useCallback(async () => {
     const id = await startLiveActivity({
       heading: "Activity in progress",
@@ -17,6 +31,9 @@ const useLiveActivity = () => {
     }
   }, []);
 
+  /**
+   * Ends the live activity and removes its ID from storage.
+   */
   const handleEndActivity = useCallback(async () => {
     const activityId = Storage.getString(liveActivityKey);
     if (activityId) {
--- utils/LiveActivity.ts
+++ utils/LiveActivity.ts
@@ -6,6 +6,12 @@ type LiveActivityParams = {
   heading: string;
 };
 
+/**
+ * Starts a live activity on iOS.
+ *
+ * @param {LiveActivityParams} params - The parameters for the live activity.
+ * @returns {Promise<string | null | undefined>} - The ID of the started live activity, or null if it failed.
+ */
 export const startLiveActivity = async (
   params: LiveActivityParams,
 ): Promise<string | null | undefined> => {
@@ -21,6 +27,13 @@ export const startLiveActivity = async (
   }
 };
 
+/**
+ * Updates an existing live activity on iOS.
+ *
+ * @param {string} activityId - The ID of the live activity to update.
+ * @param {string} heading - The new heading for the live activity.
+ * @returns {Promise<void>} - A promise that resolves when the update is complete.
+ */
 export const updateLiveActivity = async (
   activityId: string,
   heading: string,
@@ -35,6 +48,12 @@ export const updateLiveActivity = async (
   }
 };
 
+/**
+ * Ends an existing live activity on iOS.
+ *
+ * @param {string} activityId - The ID of the live activity to end.
+ * @returns {Promise<void>} - A promise that resolves when the live activity is ended.
+ */
 export const endLiveActivity = async (activityId: string): Promise<void> => {
   if (Platform.OS === "ios") {
     try {
